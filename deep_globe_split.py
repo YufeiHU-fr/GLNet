010a55ddfd5a56a93f399882292bafda0a621c74
YufeiHU-fr <56996070+YufeiHU-fr@users.noreply.github.com>
2022-01-13 10:42:39 +0100

    update

---
+++ deep_globe_split.py
@@ -0,0 +1,27 @@
+import os
+import shutil
+
+root='/home/student/workspace_Yufei/datasets/deepGlobe/train_original'
+root_val = '/home/student/workspace_Yufei/datasets/deepGlobe/crossvali'
+root_test = '/home/student/workspace_Yufei/datasets/deepGlobe/offical_crossvali'
+root_train='/home/student/workspace_Yufei/datasets/deepGlobe/train'
+
+fopen_test = open('./test.txt','r')
+lines_test = fopen_test.readlines()
+fopen_train = open('./train.txt','r')
+lines_train=fopen_train.readlines()
+
+for line in lines_train:
+    line_train_image = os.path.join(root,line)
+    line_train_label = line_train_image.replace('_sat.jpg', '_mask.png')
+    shutil.move(line_train_image,root_train)
+    shutil.move(line_train_label, root_train)
+    # print(line_train_image)
+    # print(line_train_label)
+
+for line in lines_test:
+    line_train_image = os.path.join(root,line)
+    line_train_label = line_train_image.replace('_sat.jpg', '_mask.png')
+    shutil.move(line_train_image,root_train)
+    shutil.move(line_train_label, root_train)
+
